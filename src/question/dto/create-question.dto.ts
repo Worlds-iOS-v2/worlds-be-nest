8f0d73f86f66d839fc2e14d846d59f99515b93aa
seohallo <seoohaii@gmail.com>
2025-07-15 17:24:42 +0900

    [feat] 질문생성dto

--- src/question/dto/create-question.dto.ts
+++ src/question/dto/create-question.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
+import { Category } from 'src/common/enums/category.enum';
 
 export class CreateQuestionDto {
   @IsNotEmpty()
   @ApiProperty({ description: '게시글 제목', example: '게시글 제목' })
   title: string;
+
+  @IsNotEmpty()
+  @ApiProperty({ description: '카테고리', example: 'study', enum: Category })
+  category: Category;
 
   @IsNotEmpty()
   @ApiProperty({ description: '게시글 내용', example: '게시글 내용' })
