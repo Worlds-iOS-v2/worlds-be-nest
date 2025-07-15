bba13ac7f02b6a9dbade313f8e16ce7cb4dad63e
seohallo <seoohaii@gmail.com>
2025-07-15 15:38:03 +0900

    [feat]question모듈,서비스,컨트롤러 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule, QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
