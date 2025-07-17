c163ac85be1d630f4e40a9995dc0faaaec3bd03c
seohallo <seoohaii@gmail.com>
2025-07-17 17:04:20 +0900

    [feat] 게시물신고 dto

--- src/question/dto/report-question.dto.ts
+++ src/question/dto/report-question.dto.ts
@@ -3,7 +3,7 @@ import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { ReportReason } from 'src/common/enums/report-reason.enum';
 
 export class ReportDto {
-  @ApiProperty({ description: '신고 사유', enum: ReportReason })
+  @ApiProperty({ description: '신고 사유', example: 'offensive', enum: ReportReason })
   @IsEnum(ReportReason)
   @IsNotEmpty()
   reason: ReportReason;
