25e42bb7bbaf7ee84dda9285d866d23343694110
seohallo <seoohaii@gmail.com>
2025-07-17 14:49:24 +0900

    [feat] 질문생성,목록,상세조회 module 작성

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [QuestionService],
   controllers: [QuestionController]
 })
