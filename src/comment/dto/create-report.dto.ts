e5a9265951a30799c7ffabe78fabcb553e2f1f4b
ekdmsdl <jegalmin02@gmail.com>
2025-07-21 16:51:58 +0900

    댓글 신고 comment에 합치기

--- src/comment/dto/create-report.dto.ts
+++ src/comment/dto/create-report.dto.ts
@@ -1,15 +1,14 @@
 import { IsEnum, IsOptional, IsInt } from 'class-validator';
 import { ReportReason } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateReportDto {
   @IsEnum(ReportReason)
+  @ApiProperty({ example: '욕설을 포함하고 있어요' })
   reason: ReportReason;
 
   @IsOptional()
   @IsInt()
-  commentId?: number;
-
-  @IsOptional()
-  @IsInt()
+  @ApiProperty({ example: 42 })
   questionId?: number;
 }
