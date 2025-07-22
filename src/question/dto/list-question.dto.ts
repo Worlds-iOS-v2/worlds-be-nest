c26ca5b4a0becc6e795c3b2671d5d15c063d3354
seohallo <seoohaii@gmail.com>
2025-07-23 06:13:46 +0900

    [feat] 프론트 모델 구조에 맞게 필드 추가

--- src/question/dto/list-question.dto.ts
+++ src/question/dto/list-question.dto.ts
@@ -3,6 +3,9 @@ import { Category } from 'src/common/enums/category.enum';
 
 export class ListQuestionDto {
 
+  @ApiProperty({ description: '질문 ID', example: 1 })
+  id: number;
+
   @ApiProperty({ description: '카테고리', example: 'study', enum: Category })
   category: Category;
 
@@ -12,6 +15,13 @@ export class ListQuestionDto {
   @ApiProperty({ description: '게시글 내용', example: '게시글 내용' })
   content: string;
 
+  @ApiProperty({ description: '생성일자', example: '2024-07-23T00:00:00Z' })
+  createdAt: Date;
+
+  @ApiProperty({ description: '답변 여부', example: false })
+  isAnswered: boolean;
+
   @ApiProperty({ description: '답변 개수', example: 2 })
   answerCount: number;
+  
 }
