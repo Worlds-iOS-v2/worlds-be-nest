c3238eee07cec7fe4ba7984c0d09103cfcf51eb1
seohallo <seoohaii@gmail.com>
2025-07-24 16:27:14 +0900

    [chore] azure연결

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AzureStorageModule } from 'src/common/azure-storage/azure-storage.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AzureStorageModule],
   providers: [QuestionService],
   controllers: [QuestionController]
 })
