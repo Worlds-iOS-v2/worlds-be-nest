b030e80781afbb46392f2637258b2bddb24e4c79
nayeon725 <skdus725@naver.com>
2025-08-01 08:03:01 +0900

    [style] 스웨거 문서, 프롬프트 수정

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,16 +1,29 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-    handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
-        if (err || !user) {
-            throw err || new UnauthorizedException({
-                message: ['유효한 Access token이 필요합니다.'],
-                error: 'Unauthorized',
-                statusCode: 401,
-            });
-        }
-        return user;
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): TUser {
+    if (err || !user) {
+      throw (
+        err ||
+        new UnauthorizedException({
+          message: ['사용자를 찾을 수 없습니다.'],
+          error: 'Unauthorized',
+          statusCode: 401,
+        })
+      );
     }
+    return user;
+  }
 }
