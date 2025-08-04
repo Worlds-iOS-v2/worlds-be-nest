9682140d9aca031dc25344425667d92921778aa8
seohallo <seoohaii@gmail.com>
2025-08-05 02:46:05 +0900

    [add] 채팅-인증데코레이터추가

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ApiTags, ApiParam } from '@nestjs/swagger';
 import { ParseIntPipe } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @ApiTags('Chat')
 @Controller('chat')
@@ -10,6 +12,7 @@ export class ChatController {
 
   // 채팅방 목록
   @Get('chatrooms/:userId')
+  @UseGuards(JwtAuthGuard)
   @ApiParam({ name: 'userId', type: Number, example: 2 })
   async getUserChatRooms(@Param('userId', ParseIntPipe) userId: number) {
     return this.chatService.getUserChatRooms(userId);
@@ -17,6 +20,7 @@ export class ChatController {
 
   // 채팅방 입장 -> 메세지 상세 내용
   @Get('messages/:roomId')
+  @UseGuards(JwtAuthGuard)
   @ApiParam({ name: 'roomId', type: Number })
   async getRoomMessages(
     @Param('roomId', ParseIntPipe) roomId: number,
