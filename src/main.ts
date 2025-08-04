13eb8c3c9cdfbd75e640d9d562d8918e1baf2c22
nayeon725 <skdus725@naver.com>
2025-08-05 08:32:45 +0900

    [fix] 응답 형식 불일치 버그 해결

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './common/interceptor/http-exception.filter';
-import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
