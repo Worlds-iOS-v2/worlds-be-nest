e538c52ac3eca04ef69b2d9bff88e53fe0327719
nayeon725 <skdus725@naver.com>
2025-08-07 16:28:29 +0900

    [del] 작동 안하는 크롤링 파일 삭제

--- src/crawling/crawling.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { CrawlingService } from './crawling.service';
-
-@Controller('crawling')
-export class CrawlingController {
-  constructor(private readonly crawlingService: CrawlingService) {}
-}
--- src/crawling/crawling.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CrawlingService } from './crawling.service';
-import { CrawlingController } from './crawling.controller';
-
-@Module({
-  controllers: [CrawlingController],
-  providers: [CrawlingService],
-})
-export class CrawlingModule {}
